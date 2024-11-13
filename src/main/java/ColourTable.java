fe2b806c8f6ae09691d0064c72accec419a233b6
fionnc <fionncarmody@gmail.com>
2024-11-13 15:36:31 +0000

    Wrote test for situation where no parameters are passed to constructor and developed code to handle this situation

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -4,6 +4,10 @@ public class ColourTable {
 
     int size = 0;
 
+    public ColourTable() {
+        throw new IllegalArgumentException("No parameter was passed to constructor");
+    }
+
     public ColourTable(int size) {
         if (size <= 1 || (size & (size - 1))!=0) {
             throw new IllegalArgumentException("Number entered was not a power of 2 and also greater than 1");
--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ColourTableTest {
@@ -6,5 +8,10 @@ public class ColourTableTest {
     public void testConstructorValidSize() {
         ColourTable table = new ColourTable(4);
         assertNotNull(table);
+    }
+
+    @Test
+    public void testConstructorWithoutParameterPassed() {
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable());
     }
 }
