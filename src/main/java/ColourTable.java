c874435ae35620e19d5fc2fade345c96e0aca6b7
fionnc <fionncarmody@gmail.com>
2024-11-13 12:45:21 +0000

    Test now not failing after simple implementation of ColourTable.java

---
+++ src/main/java/ColourTable.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ColourTable {
+
+    int size = 0;
+
+    public ColourTable(int size) {
+        this.size = size;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> colourPalette = new ArrayList<Integer>();;
+    }
+}
